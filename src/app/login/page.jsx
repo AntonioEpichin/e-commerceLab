0528b68e5979c0a5368979dbce9d3ca14beefb8e
AntonioEpichin <tadeuepichin@gmail.com>
2024-05-24 16:12:51 +0000

    primeira versao de auth funcionando

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,14 @@
-'use client';
+import { login, signup } from './actions'
 
-import SignIn from "@/components/SignIn";
-
-export default function Home() {
-    return (
-        <main>
-            <SignIn />
-        </main>
-    );
+export default function LoginPage() {
+  return (
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input id="email" name="email" type="email" required />
+      <label htmlFor="password">Password:</label>
+      <input id="password" name="password" type="password" required />
+      <button formAction={login}>Log in</button>
+      <button formAction={signup}>Sign up</button>
+    </form>
+  )
 }
