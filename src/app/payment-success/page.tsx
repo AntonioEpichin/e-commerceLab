ce31e58aff6c663d8fb9bc003d5ae0e2ce07a49d
AntonioEpichin <tadeuepichin@gmail.com>
2024-08-12 21:48:05 -0300

    inserido logica de enviar email na rota payment-success

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Typography, Box, Paper } from '@mui/material';
 import { auth } from 'auth';
+import { sendMail } from '@/lib/mailService';
 
 interface PaymentSuccessProps {
   searchParams: {
@@ -14,6 +15,48 @@ export default async function PaymentSuccess({ searchParams: { amount } }: Payme
 
   const {user} = await auth()
 
+  // Função para gerar um número de ordem aleatório
+  function generateOrderNumber(): string {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let orderNumber = "";
+    for (let i = 0; i < 8; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      orderNumber += characters[randomIndex];
+    }
+    return orderNumber;
+  }
+
+  const orderNumber: string = generateOrderNumber();
+
+  // Template de e-mail em HTML
+  const mailTemplate: string = `
+  <html>
+  <head>
+      <title>Sua ordem foi processada com sucesso</title>
+  </head>
+  <body>
+      <h1>Obrigado pela confiança!</h1>
+      <p>Sua ordem foi processada com sucesso.</p>
+      <p>O número da sua ordem é: <strong>${orderNumber}</strong></p>
+      <p>Estamos à disposição para qualquer dúvida.</p>
+      <p>Atenciosamente,</p>
+      <p>Equipe Comercial</p>
+  </body>
+  </html>
+  `;
+
+  // Dados do e-mail
+  const from: string = process.env.MAIL_USERNAME;
+  const to: string = user.email; // Enviar para o e-mail do usuário logado
+  const subject: string = "Sua ordem foi processada com sucesso";
+
+  // Enviar o e-mail
+  await sendMail(from, to, subject, mailTemplate);
+
+  //COLOCAR AQUI A LÓGICA PARA SALVAR NO BANCO DE DADOS
+
+  // COLOCAR UMA PROP DE USUARIO PARA PEGAR O NOME DO USUARIO E TRANSFORMAR EM UM COMPONENTE "USE CLIENT"
+
   return (
       <Container maxWidth="lg" sx={{ padding: '20px', textAlign: 'center', marginTop: '20px' }}>
         <Box sx={{ background: '#418041', padding: '40px', borderRadius: '8px', color: 'white' }}>
@@ -22,7 +65,7 @@ export default async function PaymentSuccess({ searchParams: { amount } }: Payme
               Obrigado, {user.name}!
             </Typography>
             <Typography variant="h2" component="h2" sx={{ fontSize: '1.5rem' }}>
-              Sua ordem foi processada com sucesso.
+              Sua ordem foi processada com sucesso enviaremos um e-mail com os detalhes da sua compra.
             </Typography>
 
             <Paper elevation={3} sx={{ backgroundColor: 'white', padding: '10px', borderRadius: '8px', marginTop: '20px', color: '#418041', fontSize: '2rem', fontWeight: 'bold' }}>
