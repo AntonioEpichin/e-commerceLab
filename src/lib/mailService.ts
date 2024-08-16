6b97f05b0c5a50060b085108d6bbe3fe37cff9fe
AntonioEpichin <tadeuepichin@gmail.com>
2024-08-15 23:14:08 -0300

    debug sendMaik function

--- src/lib/mailService.ts
+++ src/lib/mailService.ts
@@ -1,7 +1,6 @@
 import nodemailer from 'nodemailer';
 import winston from 'winston';
 
-
 const logger = winston.createLogger({
     level: 'debug',
     format: winston.format.json(),
@@ -9,28 +8,38 @@ const logger = winston.createLogger({
 });
 
 export const sendMail = async (from: string, to: string, subject: string, html: string) => {
-    const transporter = nodemailer.createTransport({
-        service: process.env.MAIL_HOST,
-        port: 465,
-        auth: {
-            user: process.env.MAIL_USERNAME,
-            pass: process.env.MAIL_PASSWORD
-        }
-    });
+    try {
+        logger.debug(`MAIL_HOST: ${process.env.MAIL_HOST}`);
+        logger.debug(`MAIL_USERNAME: ${process.env.MAIL_USERNAME}`);
+        logger.debug(`MAIL_PASSWORD: ${process.env.MAIL_PASSWORD ? '******' : 'Not Provided'}`);
 
-    const mailOptions = {
-        from: from,
-        to: to,
-        subject: subject,
-        html: html
-    };
+        const transporter = nodemailer.createTransport({
+            service: process.env.MAIL_HOST,
+            port: 465,
+            auth: {
+                user: process.env.MAIL_USERNAME,
+                pass: process.env.MAIL_PASSWORD
+            }
+        });
 
-    logger.info(`Sending mail to - ${to}`);
-    transporter.sendMail(mailOptions, (error, info)=> {
-        if (error) {
-            logger.error(error);
-        } else {
-            logger.info('Email sent: ' + info.response);
-        }
-    });
-}
+        const mailOptions = {
+            from: from,
+            to: to,
+            subject: subject,
+            html: html
+        };
+
+        logger.info(`Sending mail to - ${to}`);
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                logger.error(`Error sending email: ${error.message}`);
+                throw new Error(`Failed to send email: ${error.message}`);
+            } else {
+                logger.info(`Email sent successfully: ${info.response}`);
+            }
+        });
+    } catch (err) {
+        logger.error(`Unexpected error: ${err.message}`);
+        throw err;
+    }
+};
